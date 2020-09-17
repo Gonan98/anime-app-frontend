7a57c3c52291c588ad038760aedd454cf79cbc39
Gonan98 <andreg-16@hotmail.com>
2020-09-17 14:43:23 -0500

    Updates

---
+++ precache-manifest.39ad62914eaa45ca31c62e37a23f1bb8.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2823772ea04b38e0cd75f435d7f6a80a",
+    "url": "/anime-app-frontend/index.html"
+  },
+  {
+    "revision": "69ef2f8d72f4d1c3c32f",
+    "url": "/anime-app-frontend/static/css/2.75f81579.chunk.css"
+  },
+  {
+    "revision": "69ef2f8d72f4d1c3c32f",
+    "url": "/anime-app-frontend/static/js/2.5a0bb7cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/anime-app-frontend/static/js/2.5a0bb7cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0855e6a88086b8cd062",
+    "url": "/anime-app-frontend/static/js/main.cf5f0890.chunk.js"
+  },
+  {
+    "revision": "f6cb8bb3a3ee42ec1d5d",
+    "url": "/anime-app-frontend/static/js/runtime-main.274a6633.js"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/anime-app-frontend/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/anime-app-frontend/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/anime-app-frontend/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/anime-app-frontend/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/anime-app-frontend/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/anime-app-frontend/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/anime-app-frontend/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/anime-app-frontend/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/anime-app-frontend/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/anime-app-frontend/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/anime-app-frontend/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/anime-app-frontend/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/anime-app-frontend/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/anime-app-frontend/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/anime-app-frontend/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
